7b4587725d0760a3703c5cae1f372d5a3798bed6
Matthew Geiger <matt.g@castus.tv>
2024-02-19 13:53:09 -0800

    Added FindFFMPEG.cmake
    - FindFFMPEG uses pkg-config to locate ffmpeg development libraries
    - This new script is called by the main cmake script

---
+++ cmake/FindFFMPEG.cmake
@@ -0,0 +1,18 @@
+# Find FFMPEG development libraries
+
+find_package(PkgConfig)
+pkg_check_modules(FFMPEG REQUIRED IMPORTED_TARGET
+    libavcodec
+    libavformat
+    libavutil
+    libswscale
+)
+
+if (FFMPEG_FOUND)
+    message(STATUS "FFmpeg libraries found.")
+    message(STATUS "FFmpeg include directories: ${FFMPEG_INCLUDE_DIRS}")
+    message(STATUS "FFmpeg libraries: ${FFMPEG_LIBRARIES}")
+else()
+    message(FATAL_ERROR "FFmpeg libraries not found")
+endif()
+
