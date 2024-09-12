70b52f07786d652ba64114a84358d24dd0aab562
Matthew Geiger <matt.g@castus.tv>
2024-09-11 19:59:02 -0700

    Started writing multithreaded avserver

---
+++ src/mtavserver.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file mtavserver.hpp
+ * @brief Main header file for the mtavserver library
+ * @date 2024-09-11
+ * @author Matthew Todd Geiger
+ */
+
+#pragma once
+
+namespace AV::Utils {
+
+} // namespace AV::Utils
